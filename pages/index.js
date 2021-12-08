36f2c04ee5440a7dca603b8af7fb506568cd59f0
minki607 <suh@student.unimelb.edu.au>
2021-12-08 16:48:17 +0900

    feat: Feed Layout

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
       <main className="bg-[#000] min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
 
         {/* Modal */}
