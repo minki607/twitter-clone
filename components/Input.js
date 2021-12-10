b2e18a97b99bae97377199bad7798d7db6538a5c
minki607 <suh@student.unimelb.edu.au>
2021-12-10 11:46:08 +0900

    feat: Input component (Emoji added)

---
+++ components/Input.js
@@ -0,0 +1,115 @@
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
+
+const Input = () => {
+  const [input, setInput] = React.useState("");
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [showEmojis, setShowEmojis] = React.useState(false);
+  const filePickerRef = React.useRef(null);
+
+  const submitPost = () => {};
+
+  const addImageToPost = () => {
+    console.log("add");
+  };
+
+  const addEmoji = (e) => {
+    console.log(e.unified);
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
+
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+    >
+      <img
+        src="https://yt3.ggpht.com/yti/APfAmoFDv73PQp8JBBhDI6A8km6hV968Hjxtqt_sew=s88-c-k-c0x00ffffff-no-rj-mo"
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
+      <div className="w-full divide-y divide-gray-700">
+        <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
+          <textarea
+            className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
+            placeholder="What's happening?"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            rows="2"
+          />
+
+          {selectedFile && (
+            <div className="relative">
+              <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer">
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                onChange={addImageToPost}
+                ref={filePickerRef}
+                hidden
+              />
+            </div>
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+            </div>
+
+            {showEmojis && (
+              <Picker
+                onSelect={addEmoji}
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "320px",
+                  borderRadius: "20px",
+                }}
+                theme="dark"
+              />
+            )}
+          </div>
+          <button
+            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold disabled:opacity-50 disabled:cursor-default"
+            disabled={!input.trim() && !selectedFile}
+            // onClick={submitPost}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
